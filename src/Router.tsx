7eb1f772fa95c13116640c0ce6a3bfd8a7742252
itsmecamila <camilamarq.depaula@gmail.com>
2024-06-21 15:42:47 -0300

    chore: fix code

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login'
+import SignUp from './components/Register';
+import Shop from './routes/Shop';
+import Checkout from './routes/Checkout';
+import ProductDetail from './routes/ProductDetail';
+import Home from './routes/Home';
 ///Importação da biblioteca de rotas
 
 ///Função que define rotas
@@ -6,13 +12,17 @@ export default function Router(){
     return (
         <BrowserRouter>
           <Routes>
+            <Route path="/login" element={ <Login />} />
+            <Route path="/register" element={ <SignUp />} />
             <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/product" element={<SingleProduct />} />
-            <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/shop/:product" element={<ProductDetail />} />
+            {/* <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} /> */}
           </Routes>
         </BrowserRouter>
       )
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Router from './Router.tsx'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import './index.css'
+
+import Router from './Router.tsx'
+import { persistor, store } from './redux/index.ts'
 
 ///Função que renderiza todos componentes da aplicação
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router/> {/* Componente das rotas */}
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router/> {/* Componente das rotas */}
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 )
